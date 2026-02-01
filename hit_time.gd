a5334b060ca8c2f7eb7f66ae30da583f52e0743b
GenericUserIsTaken <65835587+GenericUserIsTaken@users.noreply.github.com>
2026-02-01 01:43:18 -0800

    beat time calculations

--- hit_time.gd
+++ hit_time.gd
@@ -1,10 +1,21 @@
 class_name HitTime extends Resource
 
 
-enum Type {
+enum HitType {
 	BEATA,
 	BEATB
 }#hold a,etc
 
-@export var beatTime: int
-@export var songtime: float
+@export var measure: int
+@export var subbeat: int
+@export var song_time: float
+@export var hit_type : HitType
+
+func _init(song_time = null, measure = null, subbeat = null, hit_type = null):
+	self.song_time = song_time
+	self.measure = measure
+	self.subbeat = subbeat
+	self.hit_type = hit_type
+
+func _to_string():
+		return "Hit type {0}, at measure {1}, subbeat {2}, recorded song time {3}".format([self.hit_type,self.measure,self.subbeat,self.song_time])
--- scenes/main/rhythm_node.gd
+++ scenes/main/rhythm_node.gd
@@ -2,14 +2,55 @@ extends Node3D
 '''
 bpm: 140 (4/4 time)
 104 measures of song (2:58.3) + about 2 measures of a crash cymbal at the end
-'''
-@onready var music_player : AudioStreamPlayer= self.get_node("MaskSongDemo")
 
-@export var song_time := 0 #export to see time in editor
-@export var timings : Array[HitTime] = []
+140/60 = bps
+
+140/30 * ? sec = 8 subdivisions
+(8*30)/140 = sec for one measure
+
+
+1 measure = ?
+4 beats = 1 measure
+8 subbeats = 1 measure (for convinience)
+
+measure 30 subbeat 0
+measure 30 subbeat 7
+measure 30 subbeat 8 -> measure 31 subbeat 0
+
+'''
+const _bpm := 140.0
+const _measure_length : float = (8.0*30.0)/_bpm
+const _subdivision_length : float = 30.0/_bpm
+
+@onready var music_player : AudioStreamPlayer= self.get_node("MaskSongDemo")
+@export var _song_time := 0.0 #export to see time in editor
+@export var _timings : Array[HitTime] = []
+@export var _margin := 0.8
+
+
 
 func _ready() -> void:
 	music_player.play()
+	var calctime := calc_songtime(36,4)
+	#print(calctime, " : ",calc_measure(calctime)," : ",calc_subbeat(calctime))
 
 func _process(delta: float) -> void:
-		song_time = music_player.get_playback_position()
+		_song_time = music_player.get_playback_position()
+		
+func _input(event: InputEvent) -> void:
+	if event is InputEventKey:
+		if event.pressed and event.keycode == KEY_F:
+			var newTime = HitTime.new(_song_time,calc_measure(_song_time),calc_subbeat(_song_time),HitTime.HitType.BEATA)
+			_timings.append(newTime)
+			print(newTime)
+			
+func calc_measure(song_time) -> int:
+	return 1 + (song_time/_measure_length)
+	
+func calc_subbeat(song_time) -> int:
+	var left :float = fmod(song_time,_measure_length)
+	#print(left," : ",left/_subdivision_length, " : ",_subdivision_length)
+	return left/_subdivision_length 
+	
+func calc_songtime(measure,subbeat) -> float:
+	return (measure-1)*((8.0*30.0)/_bpm) + subbeat*(30.0/_bpm)
