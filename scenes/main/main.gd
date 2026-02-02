9516d3f98d96dcbe008994a682b2197217324604
Fish <me@fishies.dev>
2026-02-01 16:46:37 -0800

    order

--- scenes/main/main.gd
+++ scenes/main/main.gd
@@ -5,36 +5,32 @@ const TestDialogue = preload("uid://r63k0f2m0rdu")
 const FirstDialogue = preload("uid://bqovvstfpyaf3")
 const MaskBuild1Dialogue = preload("uid://chqxpp1x54boe")
 const MaskBuild2Dialogue = preload("uid://0p1vsfhdw78s")
-<<<<<<< Updated upstream
 @onready var rythm_node: RhythmNode = %RythmNode
-=======
 const FinalDialogue = preload("uid://cqy5n2xlkvlw8")
->>>>>>> Stashed changes
 
 
 func _ready() -> void:
 	get_tree().root.min_size = Vector2i(1280, 720)
 
-<<<<<<< Updated upstream
 	DialogueWindow.show_dialogue(FirstDialogue.new())
 	await DialogueWindow.dialogue_ended
+
+	DialogueWindow.clear_transcript()
+	DialogueWindow.show_dialogue(MaskBuild1Dialogue.new())
+	await DialogueWindow.dialogue_ended
+
 	# TODO: Start song 1%
 	rythm_node.start_song1()
 	await rythm_node.song_end
+
+	# TODO: Start song 1
 	DialogueWindow.clear_transcript()
-	DialogueWindow.show_dialogue(MaskBuild1Dialogue.new())
+	DialogueWindow.show_dialogue(MaskBuild2Dialogue.new())
 	await DialogueWindow.dialogue_ended
+
 	# TODO: Start song 2
 	rythm_node.start_song2()
 	await rythm_node.song_end
-=======
-	#DialogueWindow.show_dialogue(FirstDialogue.new())
-	#await DialogueWindow.dialogue_ended
-	# TODO: Start song 1
->>>>>>> Stashed changes
-	DialogueWindow.clear_transcript()
-	DialogueWindow.show_dialogue(MaskBuild1Dialogue.new())
-	await DialogueWindow.dialogue_ended
 	# TODO: Start song 2
 	#DialogueWindow.clear_transcript()
 	#DialogueWindow.show_dialogue(MaskBuild2Dialogue.new())
